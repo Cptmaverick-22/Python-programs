b146797c9145b6278ab67eaa1302771adf4bf099
Sourasish Ain <sourasishain@gmail.com>
2024-04-10 00:00:41 +0530

    updated

--- NORMAL/highest-prime factor.py
+++ NORMAL/highest-prime factor.py
@@ -1,15 +1,9 @@
 num = int(input("Enter a number: "))
 fact_list = []
-prime = []
+
 for i in range(1, num+1):
     if num %i== 0:
         fact_list.append(i)
 print(fact_list)
-for n in fact_list:
-    if n > 1:
-        for j in range(2,n):
-            if n % j == 0:
-                break
-        else:
-            prime.append(n)
-print(prime)
+prime = [n for n in fact_list if n > 1 and all(n % j != 0 for j in range(2, int(n**0.5) + 1))]
+print(max(prime))
