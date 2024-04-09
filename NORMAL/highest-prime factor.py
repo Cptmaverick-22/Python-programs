029c9368dbadefc8a6d3ae7e390110c5778c217a
Sourasish Ain <sourasishain@gmail.com>
2024-04-09 23:44:12 +0530

    Updated

--- NORMAL/highest-prime factor.py
+++ NORMAL/highest-prime factor.py
@@ -5,3 +5,11 @@ for i in range(1, num+1):
     if num %i== 0:
         fact_list.append(i)
 print(fact_list)
+for n in fact_list:
+    if n > 1:
+        for j in range(2,n):
+            if n % j == 0:
+                break
+        else:
+            prime.append(n)
+print(prime)
